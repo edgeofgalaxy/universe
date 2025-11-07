00e2fd29f3d8e58ac65b17a6034112a7fa5bc264
edgeofgalaxy <threelittlebirds90@gmail.com>
2025-11-08 00:49:45 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'gu-cache-v1';
+const ASSETS = [
+  './mw.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve())))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+      return caches.open(CACHE_NAME).then(cache => { cache.put(event.request, resp.clone()); return resp; });
+    })).catch(() => caches.match('./GU-25 Q2 2.0.html'))
+  );
+});
